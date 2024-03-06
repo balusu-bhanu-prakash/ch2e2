328af0a64617ed41255b48767fe9e337a1a00481
balusu-bhanu-prakash <balusubhanuprakash091@gmail.com>
2024-03-06 14:39:11 +0530

    code

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello World! This is deployed on AWS Elastic Beanstalk."
+
+if __name__ == "__main__":
+    app.run(debug=True)
