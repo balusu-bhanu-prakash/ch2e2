6c42abd6f7542c132fc66791d835cd9d3fa1761f
Balusu Bhanu Prakash <75432576+balusu-bhanu-prakash@users.noreply.github.com>
2024-03-12 11:25:16 +0000

    8081

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
     return "Hello World! This is deployed on AWS Elastic Beanstalk."
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8081)  # Listen on all interfaces and port 8081
